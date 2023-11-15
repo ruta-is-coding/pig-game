e232dc2feb708f426bf953304a31d7d445948ccb
ruta-is-coding <rutajurg.123@gmail.com>
2023-11-15 19:35:12 +0200

    Added reset functionality and refactored the code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-//Select elements by id
+//Elements and variables
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0');
@@ -11,14 +11,32 @@ const btnHold = document.querySelector('.btn--hold');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
+let currentScore, activePlayer, isGameOn, scores;
+
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let isGameOn = true;
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  isGameOn = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diceEl.classList.add('hidden');
+};
+init();
+
+const displayCurrentScore = textContentValue => {
+  document.getElementById(`current--${activePlayer}`).textContent =
+    textContentValue;
+};
 
 const switchPlayers = () => {
   currentScore = 0;
@@ -32,18 +50,18 @@ btnRoll.addEventListener('click', function () {
   if (isGameOn) {
     // 1. Generate a random dice roll
     let dice = Math.trunc(Math.random() * 6) + 1;
+
     // 2. Display the dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
+
     // 3. Check if the dice number is not 1.
     if (dice !== 1) {
       //Add dice to the current score
       currentScore += dice;
-      document.getElementById(`current--${activePlayer}`).textContent =
-        currentScore;
+      displayCurrentScore(currentScore);
     } else {
-      document.getElementById(`current--${activePlayer}`).textContent = 0;
-      //Switch to the next player
+      displayCurrentScore(0);
       switchPlayers();
     }
   }
@@ -52,14 +70,14 @@ btnRoll.addEventListener('click', function () {
 //Clicking the hold button
 btnHold.addEventListener('click', function () {
   if (isGameOn) {
-    // 1. Add current score to the active player's score
+    // 1. Add current score to the active player's total score
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
     // 2. Check if player's score is >=100
 
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the game
       isGameOn = false;
       document
@@ -75,3 +93,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+//Reset the game
+btnNew.addEventListener('click', init);
